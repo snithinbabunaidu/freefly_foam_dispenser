959e2a9dd0ab5638829cdced4e2de621d1c35af6
snithinbabunaidu <naidu.sn@northeastern.edu>
2025-06-29 19:31:36 -0400

    [feat]: created basic react app for frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import OperatorConsole from './OperatorConsole';
+
+function App() {
+  return (
+    <div className="App">
+      <OperatorConsole />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/OperatorConsole.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function OperatorConsole() {
+  return (
+    <div>
+      <h1>Freefly Foam Dispenser</h1>
+    </div>
+  );
+}
+
+export default OperatorConsole;
