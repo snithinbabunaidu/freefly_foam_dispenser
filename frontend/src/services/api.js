0c0ab080d9b6cd3ff5feeff446ae8144c37d204b
snithinbabunaidu <naidu.sn@northeastern.edu>
2025-06-29 19:40:47 -0400

    [feat]: added api requests script

---
+++ frontend/src/services/api.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:8080';
+
+export const sendCommand = (command) => {
+  return axios.get(`${API_URL}/${command}`);
+};
+
+export const getTelemetry = () => {
+  return axios.get(`${API_URL}/telemetry`);
+};
+
+export const getMissionProgress = () => {
+  return axios.get(`${API_URL}/mission_progress`);
+};
+
+export const getBattery = () => {
+  return axios.get(`${API_URL}/battery`);
+};
+
+export const getAltitude = () => {
+    return axios.get(`${API_URL}/altitude`);
+};
+
+export const getHeading = () => {
+    return axios.get(`${API_URL}/heading`);
+};
